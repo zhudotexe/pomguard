895117715649901f8cdaed40c1592d0a61d13194
Andrew Zhu <me@andrew-zhu.com>
2020-07-04 20:41:44 -0700

    Initial commit

---
+++ bot.py
@@ -0,0 +1,33 @@
+import os
+
+import twitter
+from discord.ext.commands import Bot
+
+# auth
+TOKEN = os.environ.get("TOKEN")
+TWITTER_CONSUMER_KEY = os.environ.get("TWITTER_CONSUMER_KEY")
+TWITTER_CONSUMER_SECRET = os.environ.get("TWITTER_CONSUMER_SECRET")
+TWITTER_TOKEN_KEY = os.environ.get("TWITTER_TOKEN_KEY")
+TWITTER_TOKEN_SECRET = os.environ.get("TWITTER_TOKEN_SECRET")
+
+# config
+PREFIX = "!"
+COGS = ("cogs.timers", "cogs.lookups")
+
+
+class Pomguard(Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.twitter = twitter.Api(consumer_key=TWITTER_CONSUMER_KEY,
+                                   consumer_secret=TWITTER_CONSUMER_SECRET,
+                                   access_token_key=TWITTER_TOKEN_KEY,
+                                   access_token_secret=TWITTER_TOKEN_SECRET)
+
+
+bot = Pomguard(PREFIX)
+
+for cog in COGS:
+    bot.load_extension(cog)
+
+if __name__ == '__main__':
+    bot.run(TOKEN)
---
+++ cogs/timers.py
@@ -0,0 +1,105 @@
+from discord.ext import commands, tasks
+from discord.utils import sleep_until
+import datetime
+
+BOT_CHANNEL = 728754413686095953
+
+
+def _next_weekday(day_of_week, time):
+    date = datetime.date.today()
+    date -= datetime.timedelta(days=date.weekday() - day_of_week)
+    if date < datetime.date.today():
+        date += datetime.timedelta(days=7)
+    return datetime.datetime.combine(date, time, tzinfo=datetime.timezone.utc)
+
+
+def _next_time(time):
+    date = datetime.date.today()
+    out = datetime.datetime.combine(date, time, tzinfo=datetime.timezone.utc)
+    if out < datetime.datetime.now():
+        out += datetime.timedelta(days=1)
+    return out
+
+
+def weekly_loop(day_of_week, time):
+    async def wait_for_weekly():
+        await sleep_until(_next_weekday(day_of_week, time))
+
+    def decorator(func):
+        inst = tasks.Loop(func, seconds=0, minutes=0, hours=24 * 7,
+                          count=None, reconnect=True, loop=None)
+        inst.before_loop(wait_for_weekly)
+        return inst
+
+    return decorator
+
+
+def daily_loop(time):
+    async def wait_for_time():
+        await sleep_until(_next_time(time))
+
+    def decorator(func):
+        inst = tasks.Loop(func, seconds=0, minutes=0, hours=24,
+                          count=None, reconnect=True, loop=None)
+        inst.before_loop(wait_for_time)
+        return inst
+
+    return decorator
+
+
+class Timers(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.tuesdays.start()
+        self.saturdays.start()
+        self.daily_8pm.start()
+
+    def cog_unload(self):
+        self.tuesdays.cancel()
+        self.saturdays.cancel()
+        self.daily_8pm.cancel()
+
+    # ==== weeklies ====
+    # ---- tuesday 8am UTC ----
+    # custom deliveries
+    # doman reconstruction
+    # wondrous tails
+    @weekly_loop(1, datetime.time(hour=8))
+    async def tuesdays(self):
+        channel = self.bot.get_channel(BOT_CHANNEL)
+        if not channel:
+            return
+
+        await channel.send("Weeklies reset!\n"
+                           "- Custom Deliveries\n"
+                           "- Wondrous Tails\n"
+                           "- Doman Enclave Reconstruction")
+
+    # ---- sunday 2am UTC ----
+    # cactpot
+    # fashion report
+    @weekly_loop(6, datetime.time(hour=2))
+    async def saturdays(self):
+        channel = self.bot.get_channel(BOT_CHANNEL)
+        if not channel:
+            return
+
+        await channel.send("Weeklies reset!\n"
+                           "- Fashion Report\n"  # todo get the fashion report info
+                           "- Jumbo Cactpot")
+
+    # ==== dailies ====
+    # ---- 8pm UTC ----
+    # GC turnins
+    @daily_loop(datetime.time(hour=20))
+    async def daily_8pm(self):
+        channel = self.bot.get_channel(BOT_CHANNEL)
+        if not channel:
+            return
+
+        await channel.send("Dailies reset!\n"
+                           "- Grand Company Turnins")
+
+
+def setup(bot):
+    bot.add_cog(Timers(bot))
